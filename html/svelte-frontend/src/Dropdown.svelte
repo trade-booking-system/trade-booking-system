ceb8b3bd28a077e3f0416987b79ebf8a5c70aaa8
SamOrenbakh <sorenbak@mail.yu.edu>
2023-06-14 12:07:35 -0400

    fix centering and clean up component imports

--- html/svelte-frontend/src/Dropdown.svelte
+++ html/svelte-frontend/src/Dropdown.svelte
@@ -1,12 +1,12 @@
 <script>
-    import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
+  import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
 </script>
 
-<main>
-    <Button><Chevron>Accounts</Chevron></Button>
-    <Dropdown>
-        <DropdownItem>Account 1</DropdownItem>
-        <DropdownItem>Account 2</DropdownItem>
-        <DropdownItem>Account 3</DropdownItem>
-    </Dropdown>
-</main>
+<div class="allign_with_top">
+  <Button><Chevron>Accounts</Chevron></Button>
+  <Dropdown>
+    <DropdownItem>Account 1</DropdownItem>
+    <DropdownItem>Account 2</DropdownItem>
+    <DropdownItem>Account 3</DropdownItem>
+  </Dropdown>
+</div>
