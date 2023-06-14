e0aa70a5b4d3447bcb6ce0ff1de29ba98a3d2458
jjteitel <98434018+jjteitel@users.noreply.github.com>
2023-06-14 14:48:07 -0400

    update button spacing

--- html/svelte-frontend/src/main.js
+++ html/svelte-frontend/src/main.js
@@ -1,13 +1,8 @@
 import "./app.postcss";
 import App from "./App.svelte";
-import Dropdown from "./Dropdown.svelte"
 
 const app = new App({
   target: document.getElementById("app"),
-});
-
-const dropdown = new Dropdown({
-  target: document.getElementById("dropdown"),
 });
 
 export default app;
