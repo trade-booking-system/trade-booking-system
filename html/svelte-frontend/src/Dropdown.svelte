bdb55b56eac553c329bab42840f62515064e3989
SamOrenbakh <sorenbak@mail.yu.edu>
2023-06-14 15:05:04 -0400

    Update Dropdown.svelte

--- html/svelte-frontend/src/Dropdown.svelte
+++ html/svelte-frontend/src/Dropdown.svelte
@@ -1,25 +1,37 @@
 <script>
-    import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
+  import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
 
-    const accounts = ["Account 1", "Account 2", "Account 3"];
+  const accounts = ["Account 1", "Account 2", "Account 3"];
 
-    let selectedAccount;
+  let selectedAccount;
 
-    const selectAccount = (account) => {
-        selectedAccount = account;
-    };
+  const selectAccount = (account) => {
+    selectedAccount = account;
+  };
 </script>
 
 <div class="allign_with_top">
-    <Button><Chevron>Accounts</Chevron></Button>
-    <Dropdown>
-        {#each accounts as account}
-            <DropdownItem on:click={() => selectAccount(account)}>
-                {account}</DropdownItem
-            >
-        {/each}
-    </Dropdown>
-    {#if selectedAccount !== undefined}
-        <h1>{selectedAccount}</h1>
-    {/if}
+  <Button><Chevron>Accounts</Chevron></Button>
+  <Dropdown>
+    {#each accounts as account}
+      <DropdownItem on:click={() => selectAccount(account)}
+        >{account}</DropdownItem
+      >
+    {/each}
+  </Dropdown>
+  {#if selectedAccount !== undefined}
+    <div>
+      Current Account selected: <div class="selected_account">
+        {selectedAccount}
+      </div>
+    </div>
+  {/if}
 </div>
+
+<style>
+  .selected_account {
+    color: orangered;
+    display: inline;
+    font-weight: bold;
+  }
+</style>
