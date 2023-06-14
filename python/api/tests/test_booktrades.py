cdb3e8d32ef14ff0bfc693e9dc82e52b6a8fa7a8
ynewmark <ynewmark@mail.yu.edu>
2023-06-14 19:18:36 -0400

    add get test and helper functions

--- python/api/tests/test_booktrades.py
+++ python/api/tests/test_booktrades.py
@@ -1,3 +1,4 @@
+import random
 import routes
 import schema
 import pytest
@@ -12,6 +13,9 @@ class FakeClient:
     
     def hset(self, name, key, value):
         self.trades[key] = value
+    
+    def hvals(self, name):
+        return self.trades.values()
 
 class FakeRouter:
     def get(self, _path):
@@ -20,15 +24,49 @@ class FakeRouter:
     def put(self, _path):
         return lambda func: func
 
-def test_put(monkeypatch: pytest.MonkeyPatch):
+def trades_equal(trade1: schema.Trade, trade2: schema.Trade) -> bool:
+    for key in ["account", "type", "stock_ticker", "amount", "date"]:
+        if trade1.dict()[key] != trade2.dict()[key]:
+            return False
+    return True
+
+def initialize(monkeypatch: pytest.MonkeyPatch) -> FakeClient:
     client = FakeClient()
-    monkeypatch.setattr(routes, "r", client)
+    monkeypatch.setitem(globals(), "r", client) # TODO: change name of global
     monkeypatch.setattr(routes, "app", FakeRouter())
-    trade = schema.Trade(account="account", type="buy", stock_ticker="XYZ",
-                             amount=123, date=datetime.date.fromisoformat("20010804"))
+    return client
+
+def generate_trade(seed: int) -> schema.Trade:
+    random_gen = random.Random(seed)
+    account = "account" + random_gen.randrange(1, 4)
+    type = random_gen.choice(["buy", "sell"])
+    stock_ticker = random_gen.choice(["ABC", "XYZ", "LMNOP"])
+    amount = random_gen.randrange(1, 1000)
+    date = datetime.date(random_gen.randrange(1900, 2100), random_gen.randrange(1, 13),
+                         random_gen.randrange(1, 29))
+    return schema.Trade(account=account, type=type, stock_ticker=stock_ticker,
+                        amount=amount, date=date)
+
+@pytest.mark.parametrize("trade", [generate_trade(x) for x in range(1000, 1025)])
+def test_put(trade: schema.Trade, monkeypatch: pytest.MonkeyPatch):
+    client = initialize(monkeypatch)
     result = routes.put(trade)
-    assert result["key"] == "trades:account:20010804"
+    assert result["key"] == "trades:" + trade.account + ":" + trade.date # TODO: format
     # TODO: result["value"] should be in uuid4 format
     trade2 = client.hget("trades", result["key"])
-    for key in ["account", "type", "stock_ticker", "amount", "date"]:
-        assert trade.dict()[key] == trade2.dict()[key]
+    assert trades_equal(trade, trade2)
+
+@pytest.mark.parametrize("trade1,trade2",
+                         [(generate_trade(x), generate_trade(x + 500)) for x in range(1025, 1050)])
+def test_get(trade1: schema.Trade, trade2: schema.Trade, monkeypatch: pytest.MonkeyPatch):
+    client = initialize(monkeypatch)
+    client.hset("trades", "test1", trade1)
+    client.hset("trades", "test2", trade2)
+    trades = routes.get()
+    assert len(trades) == 2
+    if trades[0].account == "account1":
+        assert trades_equal(trades[0], trade1)
+        assert trades_equal(trades[1], trade2)
+    else:
+        assert trades_equal(trades[0], trade2)
+        assert trades_equal(trades[1], trade1)
