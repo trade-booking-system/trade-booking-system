60c0e9566e9b062f1eb96a03d3b0eb00f3fe19d9
jjteitel <98434018+jjteitel@users.noreply.github.com>
2023-06-14 14:26:41 -0400

    update account selection
    added functionality to display the account that has been selected

--- html/svelte-frontend/src/Dropdown.svelte
+++ html/svelte-frontend/src/Dropdown.svelte
@@ -1,12 +1,25 @@
 <script>
-  import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
+    import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
+
+    const accounts = ["Account 1", "Account 2", "Account 3"];
+
+    let selectedAccount;
+
+    const selectAccount = (account) => {
+        selectedAccount = account;
+    };
 </script>
 
 <div class="allign_with_top">
-  <Button><Chevron>Accounts</Chevron></Button>
-  <Dropdown>
-    <DropdownItem>Account 1</DropdownItem>
-    <DropdownItem>Account 2</DropdownItem>
-    <DropdownItem>Account 3</DropdownItem>
-  </Dropdown>
+    <Button><Chevron>Accounts</Chevron></Button>
+    <Dropdown>
+        {#each accounts as account}
+            <DropdownItem on:click={() => selectAccount(account)}>
+                {account}</DropdownItem
+            >
+        {/each}
+    </Dropdown>
+    {#if selectedAccount !== undefined}
+        <h1>{selectedAccount}</h1>
+    {/if}
 </div>
