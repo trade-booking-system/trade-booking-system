324ed6a4216ab1da6fd87e725cac1f71bd5ff039
ynewmark <ynewmark@mail.yu.edu>
2023-06-15 11:20:24 -0400

    use dependency injection for the redis client

--- python/api/utils/booktrade.py
+++ python/api/utils/booktrade.py
@@ -1,12 +1,12 @@
-from utils import redis_initializer as redis
+import redis
 import schema
 import json
 
-def booktrade(trade: schema.Trade):
+def booktrade(client: redis.Redis, trade: schema.Trade):
     key = f"trades:{trade.account}:{trade.date.isoformat()}"
     json_data= trade.json()
-    redis.r.hset(key, trade.id, json_data)
+    client.hset(key, trade.id, json_data)
     return {"Key": key, "Field": trade.id}
 
-def getTrades():
-    return (redis.r.hgetall(key) for key in redis.r.keys("*"))
+def getTrades(client: redis.Redis):
+    return (client.hgetall(key) for key in client.keys("*"))
