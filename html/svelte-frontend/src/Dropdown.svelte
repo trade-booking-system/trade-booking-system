7df1b0bfe0a0b5d5420a0d95499293879be9e7ca
jjteitel <98434018+jjteitel@users.noreply.github.com>
2023-06-16 00:35:26 -0400

    accounts update on page load

--- html/svelte-frontend/src/Dropdown.svelte
+++ html/svelte-frontend/src/Dropdown.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from "svelte";
   import { Button, Dropdown, DropdownItem, Chevron } from "flowbite-svelte";
 
   const accounts = [];
@@ -12,29 +13,33 @@
   let responseData;
 
   async function fetchData() {
-      try {
-        const response = await fetch('/api/accounts/');
-      
-        if (response.ok) {
-          responseData = await response.json();
-          responseData.forEach(element => {
-            accounts.push(element);
-          });
+    try {
+      const response = await fetch("/api/accounts/");
+
+      if (response.ok) {
+        responseData = await response.json();
+        console.log(responseData);
+        responseData.forEach((element) => {
+          accounts.push(element);
+        });
       } else {
-          console.error('Error:', response.status);
+        console.error("Error:", response.status);
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   }
+
+  onMount(fetchData);
 </script>
 
 <div class="allign_with_top">
-  <Button on:click={fetchData}><Chevron>Accounts</Chevron></Button>
-  <Dropdown>
+  <Button><Chevron>Accounts</Chevron></Button>
+  <Dropdown class="w-48 overflow-y-auto py-1 h-48">
     {#each accounts as account}
-      <DropdownItem on:click={() => selectAccount(account)}
-        >{account}</DropdownItem
+      <DropdownItem
+        class="flex items-center text-base font-semibold gap-2"
+        on:click={() => selectAccount(account)}>{account}</DropdownItem
       >
     {/each}
   </Dropdown>
