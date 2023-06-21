34c889f0b0ba3343ebce61d8872d58232fdfb36b
eitan <eitanleitner@gmail.com>
2023-06-21 14:05:10 -0400

    fixed postions

--- python/api/positions.py
+++ python/api/positions.py
@@ -8,11 +8,11 @@ from utils.get_positions import get_positions, get_position
 
 router = APIRouter()
 
-@router.get("/getPositions/{account}")
+@router.get("/{account}")
 async def get_account_positions(account: str, client: Redis = Depends(get_redis_client)) -> PositionResponse:
     positions = get_positions(client, account)
     return PositionResponse(positions=positions, count=len(positions))
 
-@router.get("/getPosition/{account}/{ticker}")
+@router.get("/{account}/{ticker}")
 async def get_ticker_position(account: str, ticker: str, client: Redis = Depends(get_redis_client)) -> Position:
     return get_position(client, account, ticker)
