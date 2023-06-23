26d8cfc218d8a7752e8c6121985f40fa0c36be15
Yoni Stern <jstern10@mail.yu.edu>
2023-06-23 12:29:33 -0400

    websocket fix

--- python/api/websocket.py
+++ python/api/websocket.py
@@ -1,8 +1,9 @@
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter, Depends
+from utils.redis_initializer import get_redis_client_zero
+import json
 from time import time
 import asyncio
 import redis
-from utils.redis_initializer import get_redis_client_zero
 
 router = APIRouter()
 
@@ -16,30 +17,26 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         print("Client disconnected")
 
-
 @router.websocket("/watchTrades")
 async def websocket_endpoint_watchTrade(websocket: WebSocket, redis: redis.Redis = Depends(get_redis_client_zero)):
     await websocket.accept()
-    pubsub = redis.pubsub()
+    pubsub = redis.pubsub(ignore_subscribe_messages= True)
     queue = asyncio.Queue()
 
     def handler(message):
-      #  print("message: ", message)
-        if message:
-            type, trade = message["data"].split(":", 1)
-            queue.put_nowait({"type": type, "trade": trade}) 
-            
-
-    pubsub.subscribe(tradeUpdates = handler)
-    pubsub.run_in_thread()
+        type, trade = message["data"].split(":", 1)
+        data = {"type": type, "trade": trade}
+        queue.put(json.dump(data))
+    
+    pubsub.subscribe(**{"tradeUpdates":handler})
+    pubsub.run_in_thread(daemon= True)
 
     try:
         while True:
             await websocket.send_text(f"current time: {time()}")
-            if not queue.empty():
-                data = await queue.get()
+            while not queue.empty():
+                data = queue.get_nowait()
                 await websocket.send_json(data)
             await asyncio.sleep(1)
-    
     except WebSocketDisconnect:
         print("Client disconnected")
