e8852b93b7c82bd7b43dd4bdcd0ddadbe30fe786
Yoni Stern <jstern10@mail.yu.edu>
2023-06-26 14:11:04 -0400

    add caching to positions listener

--- python/aggregate/aggregate.py
+++ python/aggregate/aggregate.py
@@ -6,14 +6,24 @@ import os
 
 def trade_handler(msg):
     data= msg["data"]
-    account, stock_ticker, value= data.split(":")
-    value= int(value)
+    account, stock_ticker, amount= data.split(":")
+    update_position(account, stock_ticker, int(amount))
+
+def update_position(account, stock_ticker, amount):
     key = "positions:"+account
-    json_old_position = client.hget(key, stock_ticker)
-    old_value= 0
-    if json_old_position != None:
-        old_value= Position.parse_raw(json_old_position).amount
-    position= Position(account= account, stock_ticker= stock_ticker, amount= old_value+value, 
+    stock_tickers= positions_cache.get(account)
+    if stock_tickers == None:
+        stock_tickers= dict()
+        positions_cache[account]= stock_tickers
+    old_amount= stock_tickers.get(stock_ticker)
+    if old_amount == None:
+        position_json= client.hget(key, stock_ticker)
+        old_amount= 0
+        if position_json != None:
+            old_amount= Position.parse_raw(position_json).amount
+    new_amount= old_amount+amount
+    stock_tickers[stock_ticker]= new_amount
+    position= Position(account= account, stock_ticker= stock_ticker, amount= new_amount, 
                        last_aggregation_time= datetime.now(), last_aggregation_host= "host")
     client.hset(key, stock_ticker, position.json())
 
@@ -22,6 +32,8 @@ def termination_handler(signum, frame):
     thread.join()
     sub.close()
     client.close()
+
+positions_cache= dict()
 
 client = Redis(host = os.getenv("REDIS_HOST"), port = 6379, db = 1, decode_responses = True)
 sub= client.pubsub(ignore_subscribe_messages= True)
