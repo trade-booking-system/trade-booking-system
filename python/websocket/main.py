1cba4ecc30790b12f8a663139e2e1760478716dd
ynewmark <ynewmark@mail.yu.edu>
2023-06-30 14:55:23 -0400

    websocket takes a comma seperated list of accounts

--- python/websocket/main.py
+++ python/websocket/main.py
@@ -7,20 +7,20 @@ from .manager import ConnectionManager, get_manager
 app = FastAPI(dependencies=[Depends(get_manager)])
 
 @app.websocket("/positions")
-async def watch_positions(websocket: WebSocket, manager: ConnectionManager = Depends(get_manager("positionUpdates"))):
-    await loop(websocket, manager)
+async def watch_positions(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_manager("positionUpdates"))):
+    await loop(websocket, manager, accounts)
 
 @app.websocket("/trades")
-async def watch_trades(websocket: WebSocket, manager: ConnectionManager = Depends(get_manager("tradeUpdates"))):
-    await loop(websocket, manager)
+async def watch_trades(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_manager("tradeUpdates"))):
+    await loop(websocket, manager, accounts)
 
-async def loop(websocket: WebSocket, manager: ConnectionManager):
+async def loop(websocket: WebSocket, manager: ConnectionManager, accounts: str):
     await websocket.accept()
     queue: Queue = Queue()
-    await manager.subscribe(queue)
+    id = await manager.subscribe(queue, accounts)
     try:
         while True:
             data = await queue.get()
             await websocket.send_json(data)
     except WebSocketDisconnect:
-        manager.unsubscribe(queue)
+        manager.unsubscribe(id)
--- python/websocket/manager.py
+++ python/websocket/manager.py
@@ -1,31 +1,52 @@
 from asyncio import Queue
 import json
 
-from fastapi import WebSocket, Depends
+from fastapi import Depends
 from redis import Redis
 
 from utils.redis_initializer import get_redis_client
 
+class Connection:
+    def __init__(self, queue: Queue, accounts: str):
+        self.queue = queue
+        if accounts == "*":
+            self.accounts = []
+        else:
+            self.accounts = accounts.split(",")
+    
+    def has_account(self, account: str) -> bool:
+        if self.accounts == []:
+            return True
+        else:
+            return account in self.accounts
+    
+    def add_message(self, data):
+        self.queue.put_nowait(data)
+
 class ConnectionManager:
     def __init__(self, client: Redis, channel: str):
-        self.subsribers: list[Queue] = []
         self.queue: Queue = Queue()
-        self.connections: list[WebSocket] = []
+        self.connections: dict[int, Connection] = {}
         self.pubsub = client.pubsub()
         self.pubsub.run_in_thread()
         self.pubsub.subscribe(**{channel: self._handler})
+        self.key = 0
     
-    async def subscribe(self, queue: Queue):
-        self.subsribers.append(queue)
+    async def subscribe(self, queue: Queue, accounts: str) -> int:
+        self.connections[self.key] = Connection(queue, accounts)
+        self.key += 1
+        return self.key - 1
     
-    async def unsubscribe(self, queue: Queue):
-        self.subsribers.remove(queue)
+    async def unsubscribe(self, id: int):
+        del self.connections[id]
     
     def _handler(self, message):
         type, payload = message["data"].split(":", 1)
-        data = {"type": type, "payload": json.loads(payload)}
-        for queue in self.subsribers:
-            queue.put_nowait(data)
+        data = json.loads(payload)
+        account = data["account"]
+        for connection in self.connections.values():
+            if connection.has_account(account):
+                connection.add_message({"type": type, "payload": data})
 
 def get_manager(channel: str) -> ConnectionManager:
     def sub(client: Redis = Depends(get_redis_client)):
