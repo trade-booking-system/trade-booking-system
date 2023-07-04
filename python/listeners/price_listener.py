77dd35876823028fd4a7f08d0b302b72a0aa8c74
ynewmark <ynewmark@mail.yu.edu>
2023-07-04 00:57:18 -0400

    adds option to turn off after hours test

--- python/listeners/price_listener.py
+++ python/listeners/price_listener.py
@@ -26,6 +26,8 @@ class PriceHandler:
         return stock_price
     
     def is_market_open(self) -> bool:
+        if self.now == None:
+            return True
         date_time= self.now()
         day= date_time.weekday()
         if day == 5 or day == 6:
@@ -56,7 +58,8 @@ if __name__ == "__main__":
     client = redis.Redis(host = os.getenv("REDIS_HOST"), port = 6379, db = 0, decode_responses= True)
     signal.signal(signal.SIGTERM, termination_handler)
     tickers= ValidTickers("utils/ListOfStocks.txt")
-    handler = PriceHandler(client, tickers.get_all_tickers(), get_live_price, datetime.datetime.now)
+    handler = PriceHandler(client, tickers.get_all_tickers(), get_live_price,
+        datetime.datetime.now if os.getenv("DEBUG_MODE", "off") == "off" else None)
     while True:
         handler.update_stock_prices()
         client.publish("prices", "updated")
