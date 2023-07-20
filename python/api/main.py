f5db9c6cc38414d48d805e41000b11b4dd58889d
Yoni Stern <jstern10@mail.yu.edu>
2023-07-20 18:19:26 -0400

    add startup date

--- python/api/main.py
+++ python/api/main.py
@@ -1,27 +1,20 @@
-from fastapi import FastAPI, Depends
-from time import sleep
 from utils.redis_initializer import get_redis_client
-from api import routes
+from fastapi import FastAPI, Depends
+from datetime import datetime
 from api import positions
 from api import websocket
 from api import pl_routes
+from api import routes
 
+def set_startup_date():
+    client= get_redis_client()
+    key= "startupDate"
+    if client.get(key) == None:
+        client.set(key, datetime.now().date().isoformat())
+
+set_startup_date()
 app = FastAPI(dependencies=[Depends(get_redis_client)])
 app.include_router(routes.router)
 app.include_router(positions.router, prefix="/positions")
 app.include_router(websocket.router, prefix="/ws")
 app.include_router(pl_routes.router, prefix="/pl")
-
-
-@app.get("/sum/")
-def sum(a: int, b: int):
-    return {"sum": a + b}
-
-@app.get("/echo/")
-def echo(text: str, delay: int):
-    sleep(delay / 1000.0)
-    return {"text": text}
-
-@app.get("/hello")
-async def hello():
-    return {"hello": "hello"}
