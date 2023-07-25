9dda26be16337f2292f3dc3789332243b9974056
ynewmark <ynewmark@mail.yu.edu>
2023-07-25 10:33:35 -0400

    missed file to commit

--- python/websocket/main.py
+++ python/websocket/main.py
@@ -2,16 +2,16 @@ from asyncio import Queue
 
 from fastapi import FastAPI, Depends, WebSocket, WebSocketDisconnect
 
-from .manager import ConnectionManager, get_manager
+from .manager import ConnectionManager, get_trade_manager, get_position_manager
 
-app = FastAPI(dependencies=[Depends(get_manager)])
+app = FastAPI()
 
 @app.websocket("/positions")
-async def watch_positions(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_manager("positionUpdatesWS"))):
+async def watch_positions(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_position_manager())):
     await loop(websocket, manager, accounts)
 
 @app.websocket("/trades")
-async def watch_trades(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_manager("tradeUpdatesWS"))):
+async def watch_trades(websocket: WebSocket, accounts: str = "*", manager: ConnectionManager = Depends(get_trade_manager())):
     await loop(websocket, manager, accounts)
 
 async def loop(websocket: WebSocket, manager: ConnectionManager, accounts: str):
